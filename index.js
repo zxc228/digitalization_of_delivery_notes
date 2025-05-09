34000854c29f03978a6ec45129f622c54a89f14a
zxc228 <ilya423@yandex.ru>
2025-05-09 22:44:58 +0200

    fixed dirs bug

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
+const express    = require('express');
+const morgan     = require('morgan');
+const fs         = require('fs');
+const path       = require('path');
 const fileLogger = require('./utils/fileLogger');
 const { swaggerUi, swaggerSpec } = require('./config/swagger');
-const routes = require('./routes');
+const routes     = require('./routes');
 
 const app = express();
+
+// Ensure upload directories exist
+const UPLOAD_DIR = process.env.UPLOAD_DIR || path.join(__dirname, 'uploads');
+const TMP_DIR    = path.join(UPLOAD_DIR, 'tmp');
+
+if (!fs.existsSync(UPLOAD_DIR)) fs.mkdirSync(UPLOAD_DIR, { recursive: true });
+if (!fs.existsSync(TMP_DIR))    fs.mkdirSync(TMP_DIR, { recursive: true });
 
 // Middleware
 app.use(morgan('dev'));
 app.use(express.json());
 
 // Static files
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(UPLOAD_DIR));
 
 // Swagger docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -28,9 +37,6 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Internal Server Error' });
 });
-
-
-
 
 module.exports = app;
 
